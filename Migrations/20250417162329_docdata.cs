7e9246a5e7475205156daebdc481850817cf08e3
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2025-04-18 12:28:14 +0330

    Authentication fix.

---
+++ Migrations/20250417162329_docdata.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jaahub.Migrations
+{
+    /// <inheritdoc />
+    public partial class docdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
