609bf716fa31305cbf66fd09d20fd3bde7fdf434
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2025-07-31 02:18:33 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,20 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
